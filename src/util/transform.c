f2cfeb81fee4ff4beb224e3c316a473f79b76e7a
Geoff <geoff.nagy@gmail.com>
2016-03-28 14:12:22 -0700

    matrix multiplication

--- src/util/transform.c
+++ src/util/transform.c
@@ -1,5 +1,7 @@
 #include "util/transform.h"
 
+static Mat4 matrixMultiply(Mat4 *a, Mat4 *b);
+
 Mat4 identity()
 {
     Mat4 result = {.cols[0] = {.x = 1.0, .y = 0.0, .z = 0.0, .w = 1.0},
@@ -27,4 +29,26 @@ Mat4 rotate(Mat4 mat, float degrees)
 Mat4 scale(Mat4 mat, float x, float y)
 {
 
+}
+
+static Mat4 matrixMultiply(Mat4 a, Mat4 b)
+{
+	Mat4 c;
+    c.cols[0][0] = a -> cols[0][0] * b -> cols[0][0] + a -> cols[0][1] * b -> cols[1][0] + a -> cols[0][2] * b -> cols[2][0] + a -> cols[0][3] * b -> cols[3][0];
+    c.cols[0][1] = a -> cols[0][0] * b -> cols[0][1] + a -> cols[0][1] * b -> cols[1][1] + a -> cols[0][2] * b -> cols[2][1] + a -> cols[0][3] * b -> cols[3][1];
+    c.cols[0][2] = a -> cols[0][0] * b -> cols[0][2] + a -> cols[0][1] * b -> cols[1][2] + a -> cols[0][2] * b -> cols[2][2] + a -> cols[0][3] * b -> cols[3][2];
+    c.cols[0][3] = a -> cols[0][0] * b -> cols[0][3] + a -> cols[0][1] * b -> cols[1][3] + a -> cols[0][2] * b -> cols[2][3] + a -> cols[0][3] * b -> cols[3][3];
+    c.cols[1][0] = a -> cols[1][0] * b -> cols[0][0] + a -> cols[1][1] * b -> cols[1][0] + a -> cols[1][2] * b -> cols[2][0] + a -> cols[1][3] * b -> cols[3][0];
+    c.cols[1][1] = a -> cols[1][0] * b -> cols[0][1] + a -> cols[1][1] * b -> cols[1][1] + a -> cols[1][2] * b -> cols[2][1] + a -> cols[1][3] * b -> cols[3][1];
+    c.cols[1][2] = a -> cols[1][0] * b -> cols[0][2] + a -> cols[1][1] * b -> cols[1][2] + a -> cols[1][2] * b -> cols[2][2] + a -> cols[1][3] * b -> cols[3][2];
+    c.cols[1][3] = a -> cols[1][0] * b -> cols[0][3] + a -> cols[1][1] * b -> cols[1][3] + a -> cols[1][2] * b -> cols[2][3] + a -> cols[1][3] * b -> cols[3][3];
+    c.cols[2][0] = a -> cols[2][0] * b -> cols[0][0] + a -> cols[2][1] * b -> cols[1][0] + a -> cols[2][2] * b -> cols[2][0] + a -> cols[2][3] * b -> cols[3][0];
+    c.cols[2][1] = a -> cols[2][0] * b -> cols[0][1] + a -> cols[2][1] * b -> cols[1][1] + a -> cols[2][2] * b -> cols[2][1] + a -> cols[2][3] * b -> cols[3][1];
+    c.cols[2][2] = a -> cols[2][0] * b -> cols[0][2] + a -> cols[2][1] * b -> cols[1][2] + a -> cols[2][2] * b -> cols[2][2] + a -> cols[2][3] * b -> cols[3][2];
+    c.cols[2][3] = a -> cols[2][0] * b -> cols[0][3] + a -> cols[2][1] * b -> cols[1][3] + a -> cols[2][2] * b -> cols[2][3] + a -> cols[2][3] * b -> cols[3][3];
+    c.cols[3][0] = a -> cols[3][0] * b -> cols[0][0] + a -> cols[3][1] * b -> cols[1][0] + a -> cols[3][2] * b -> cols[2][0] + a -> cols[3][3] * b -> cols[3][0];
+    c.cols[3][1] = a -> cols[3][0] * b -> cols[0][1] + a -> cols[3][1] * b -> cols[1][1] + a -> cols[3][2] * b -> cols[2][1] + a -> cols[3][3] * b -> cols[3][1];
+    c.cols[3][2] = a -> cols[3][0] * b -> cols[0][2] + a -> cols[3][1] * b -> cols[1][2] + a -> cols[3][2] * b -> cols[2][2] + a -> cols[3][3] * b -> cols[3][2];
+    c.cols[3][3] = a -> cols[3][0] * b -> cols[0][3] + a -> cols[3][1] * b -> cols[1][3] + a -> cols[3][2] * b -> cols[2][3] + a -> cols[3][3] * b -> cols[3][3];
+    return C;
 }
