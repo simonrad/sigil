d3785ec3275c973fef733e5df913caf619c0145d
Geoff <geoff.nagy@gmail.com>
2016-05-02 02:49:51 +0000

    compile sigil on rpi

---
+++ build-rpi/CMakeFiles/sigil.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/sigil/src/internal/circle.c" "/home/pi/sigil/build-rpi/CMakeFiles/sigil.dir/src/internal/circle.c.o"
+  "/home/pi/sigil/src/internal/line.c" "/home/pi/sigil/build-rpi/CMakeFiles/sigil.dir/src/internal/line.c.o"
+  "/home/pi/sigil/src/internal/point.c" "/home/pi/sigil/build-rpi/CMakeFiles/sigil.dir/src/internal/point.c.o"
+  "/home/pi/sigil/src/internal/rectangle.c" "/home/pi/sigil/build-rpi/CMakeFiles/sigil.dir/src/internal/rectangle.c.o"
+  "/home/pi/sigil/src/internal/shaders.c" "/home/pi/sigil/build-rpi/CMakeFiles/sigil.dir/src/internal/shaders.c.o"
+  "/home/pi/sigil/src/internal/sound.c" "/home/pi/sigil/build-rpi/CMakeFiles/sigil.dir/src/internal/sound.c.o"
+  "/home/pi/sigil/src/internal/sprite.c" "/home/pi/sigil/build-rpi/CMakeFiles/sigil.dir/src/internal/sprite.c.o"
+  "/home/pi/sigil/src/internal/text.c" "/home/pi/sigil/build-rpi/CMakeFiles/sigil.dir/src/internal/text.c.o"
+  "/home/pi/sigil/src/internal/triangle.c" "/home/pi/sigil/build-rpi/CMakeFiles/sigil.dir/src/internal/triangle.c.o"
+  "/home/pi/sigil/src/libdrawtext/drawgl.c" "/home/pi/sigil/build-rpi/CMakeFiles/sigil.dir/src/libdrawtext/drawgl.c.o"
+  "/home/pi/sigil/src/libdrawtext/font.c" "/home/pi/sigil/build-rpi/CMakeFiles/sigil.dir/src/libdrawtext/font.c.o"
+  "/home/pi/sigil/src/libdrawtext/utf8.c" "/home/pi/sigil/build-rpi/CMakeFiles/sigil.dir/src/libdrawtext/utf8.c.o"
+  "/home/pi/sigil/src/sl.c" "/home/pi/sigil/build-rpi/CMakeFiles/sigil.dir/src/sl.c.o"
+  "/home/pi/sigil/src/soil/SOIL.c" "/home/pi/sigil/build-rpi/CMakeFiles/sigil.dir/src/soil/SOIL.c.o"
+  "/home/pi/sigil/src/soil/image_DXT.c" "/home/pi/sigil/build-rpi/CMakeFiles/sigil.dir/src/soil/image_DXT.c.o"
+  "/home/pi/sigil/src/soil/image_helper.c" "/home/pi/sigil/build-rpi/CMakeFiles/sigil.dir/src/soil/image_helper.c.o"
+  "/home/pi/sigil/src/soil/stb_image_aug.c" "/home/pi/sigil/build-rpi/CMakeFiles/sigil.dir/src/soil/stb_image_aug.c.o"
+  "/home/pi/sigil/src/util/images.c" "/home/pi/sigil/build-rpi/CMakeFiles/sigil.dir/src/util/images.c.o"
+  "/home/pi/sigil/src/util/shader.c" "/home/pi/sigil/build-rpi/CMakeFiles/sigil.dir/src/util/shader.c.o"
+  "/home/pi/sigil/src/util/transform.c" "/home/pi/sigil/build-rpi/CMakeFiles/sigil.dir/src/util/transform.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/include/freetype2"
+  "/usr/include/AL"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
