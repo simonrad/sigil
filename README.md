617808ea52612e7788fafa18caae33a3cf021ba7
Geoff <geoff.nagy@gmail.com>
2016-06-12 02:21:24 -0700

    add and use build scripts for mingw32 and msvc

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 Sound, Input, and Graphics Integration Library (SIGIL)
+------------------------------------------------------
 
 A simple, minimalist library for playing audio, handling basic input, and drawing 2D graphics such as sprites, shapes, and text.
+
+Folder Structure:
+-----------------
+
+examples: contains stand-alone example projects, with project files/makefiles for every single platform/compiler that SIGIL supports
+
+glew: contains headers and pre-compiled libs for the OpenGL Extension Wrangler library, which SIGIL depends on
+
+glfw: contains headers and pre-compiled libs for the GLFW library, which SIGIL depends on
+
+include: contains the "sl.h" header file you need to include in your applications to use SIGIL
+
+scripts: contains scripts designed to simplify the mass-build process I use; intended for my use only (but feel free to use it)
+
+sigil-XXXXXX: contains DLLs and static libs for SIGIL built using various compilers
+
+src: SIGIL source code, as well as some source code from some open-source libraries that SIGIL depends on
+
+Building from Source
+--------------------
+
+Conveniently, build instructions are located in this directory with the name BUILD-XXXXXX.txt for every platform/compiler that SIGIL supports.
+
+License Information
+-------------------
+
+View the LICENSE.txt file for license information. And dear Christ, is that a dry read.
+
+Contact
+-------
+
+Questions, comments, and mildly-rude sarcasm may be directed towards Geoff Nagy at geoff@libsigil.com.
--- scripts/build-mingw32.bat
+++ scripts/build-mingw32.bat
@@ -1,5 +1,5 @@
 :: a script for simplifying my life when building and distributing a new version of SIGIL;
-:: in short, it invokes the build processes for all of my Windows-based distributions and
+:: in short, it invokes the build processes for all of my MinGW-based distributions and
 :: then copies the resulting libs into the pre-compiled libraries folder and the examples,
 :: and then finally, builds the examples
 :: Geoff Nagy
@@ -7,28 +7,24 @@
 
 @echo off
 
-:: these define the locations of the MSVC command-line build utilities I use
-::set vc2010_compiler="C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe"
-::set vc2012_compiler="C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\wdexpress.exe"
-::set vc2013_compiler="C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
-::set vc2015_compiler="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe"
+:: simple alias to Codeblocks to reduce clutter below
+set codeblocks="C:\Program Files (x86)\CodeBlocks\codeblocks.exe"
 
-REM intro
-echo building SIGIL with mingw...
+:: building SIGIL with MinGW is easier than with MSVC...
+echo ------------------------------
+echo building SIGIL with MinGW32...
 
-REM cd ../build-mingw32
-REM mingw32-make clean
-REM mingw32-make
+:: force a rebuild of the entire SIGIL lib
+cd ../build-mingw32
+mingw32-make clean
+mingw32-make
+cd ../
 
-call :build_msvc_sigil vc2010 "%VS100COMNTOOLS%vsvars32.bat"
-:: call :build_msvc_sigil vc2012 "%VS110COMNTOOLS%"\\vsvars32.bat
-:: call :build_msvc_sigil vc2013 "%VS120COMNTOOLS%"\\vsvars32.bat
-:: call :build_msvc_sigil vc2015 "%VS140COMNTOOLS%"\\vsvars32.bat
+:: copy our libraries into the pre-compiled SIGIL folders and example folders
+call :copy_mingw_libraries mingw32
 
-:: call :build_msvc_sigil vc2010 %vc2010_compiler%
-:: call :build_msvc_sigil vc2012 %vc2012_compiler%
-:: call :build_msvc_sigil vc2013 %vc2013_compiler%
-:: call :build_msvc_sigil vc2015 %vc2015_compiler%
+:: lastly, rebuild our examples for MinGW
+call :build_mingw_examples mingw32
 
 :: and we're done!
 echo ----------------------------
@@ -36,70 +32,47 @@ echo done
 exit /B %ERRORLEVEL%
 
 :: ------------------------------------------------------------------------------
-:: args: name of compiler (e.g., vc2010), and environment vars file
-:build_msvc_sigil
-
-	:: print some helpful message
-	echo ------------------------------
-	echo building SIGIL with %1...
-
-	:: force a rebuild of the entire SIGIL lib
-	call %2 >nul
-	msbuild.exe ../build-%1/sigil.sln /t:Rebuild /property:Configuration=Release >nul
-
-	:: copy our libraries into the pre-compiled SIGIL folders and examples folders
-	call :copy_msvc_libraries %1
-
-	:: lastly, rebuild our examples for this compiler
-	call :build_msvc_examples %1 %2
-
+:: args: name of compiler
+:copy_mingw_libraries
+	call :copy_mingw_to_precomps %1
+	call :copy_mingw_to_example %1 input
+	call :copy_mingw_to_example %1 shapes
+	call :copy_mingw_to_example %1 sound
+	call :copy_mingw_to_example %1 sprites
+	call :copy_mingw_to_example %1 text
+	call :copy_mingw_to_example %1 transformations
 exit /B 0
 
 :: ------------------------------------------------------------------------------
-:: args: none
-:copy_msvc_libraries
-	call :copy_msvc_to_precomps %1
-	call :copy_msvc_to_example %1 input
-	call :copy_msvc_to_example %1 shapes
-	call :copy_msvc_to_example %1 sound
-	call :copy_msvc_to_example %1 sprites
-	call :copy_msvc_to_example %1 text
-	call :copy_msvc_to_example %1 transformations
+:: args: name of build (e.g., mingw32)
+:copy_mingw_to_precomps
+	echo copying libsigil.dll and libsigil.dll.a to precompiled %1 lib folder...
+	copy build-%1\libsigil.dll sigil-%1\libsigil.dll >nul
+	copy build-%1\libsigil.dll.a sigil-%1\lib\libsigil.dll.a >nul
 exit /B 0
 
 :: ------------------------------------------------------------------------------
-:: args: name of build (e.g., vc2010)
-:copy_msvc_to_precomps
-	echo copying sigil.dll and sigil.lib to precompiled %1 lib folder...
-	copy ..\build-%1\Release\sigil.dll ..\sigil-%1\sigil.dll >nul
-	copy ..\build-%1\Release\sigil.lib ..\sigil-%1\lib\sigil.lib >nul
+:: args: name of build (e.g., mingw32) and name of example (e.g., input)
+:copy_mingw_to_example
+	echo copying libsigil.dll and libsigil.dll.a to example %2 for %1...
+	copy build-%1\libsigil.dll examples\%2\%1\libsigil.dll >nul
+	copy build-%1\libsigil.dll.a examples\%2\%1\lib\libsigil.dll.a >nul
 exit /B 0
 
 :: ------------------------------------------------------------------------------
-:: args: name of build (e.g., vc2010) and name of example (e.g., input)
-:copy_msvc_to_example
-	echo copying sigil.dll and sigil.lib to example %2 for %1...
-	copy ..\build-%1\Release\sigil.dll ..\examples\%2\%1\sigil.dll >nul
-	copy ..\build-%1\Release\sigil.lib ..\examples\%2\%1\lib\sigil.lib >nul
+:: args: name of build (e.g., mingw32), and environment vars file
+: build_mingw_examples
+	call :build_mingw_example %1 input
+	call :build_mingw_example %1 shapes
+	call :build_mingw_example %1 sound
+	call :build_mingw_example %1 sprites
+	call :build_mingw_example %1 text
+	call :build_mingw_example %1 transformations
 exit /B 0
 
 :: ------------------------------------------------------------------------------
-:: args: name of build (e.g., vc2010), and environment vars file
-: build_msvc_examples
-	call :build_msvc_example %1 %2 input
-	call :build_msvc_example %1 %2 shapes
-	call :build_msvc_example %1 %2 sound
-	call :build_msvc_example %1 %2 sprites
-	call :build_msvc_example %1 %2 text
-	call :build_msvc_example %1 %2 transformations
-exit /B 0
-
-:: ------------------------------------------------------------------------------
-:: args: name of build (e.g., vc2010), environment vars file, and example name
-:build_msvc_example
-	echo building example %3 for %1...
-	call %2 >nul
-	msbuild.exe ../examples/%3/%1/%3-%1.sln /t:Rebuild /property:Configuration=Debug	
-
-::%2 ../examples/%3/%1/%3-%1.sln /Rebuild "Release"
+:: args: name of build (e.g., mingw32), and example name
+:build_mingw_example
+	echo building example %2 for %1...
+	%codeblocks% --no-splash-screen --rebuild --target="Release" examples/%2/%1/%2-%1.cbp
 exit /B 0
